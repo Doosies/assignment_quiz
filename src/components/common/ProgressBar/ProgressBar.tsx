460e5c8d697a59536b491547aa8e1fdbe3e97f61
Song_Minhyung <song961003@gmail.com>
2024-03-06 20:24:55 +0900

    deging: progressbar text white로 변경

--- src/components/common/ProgressBar/ProgressBar.tsx
+++ src/components/common/ProgressBar/ProgressBar.tsx
@@ -8,7 +8,7 @@ interface ProgressBarProps {
 export function ProgressBar({ now, max, onlyText, className }: ProgressBarProps) {
   return (
     <div className={`relative p-2  h-16 flex-center w-full ${className}`}>
-      <div className="absolute flex-center w-full">
+      <div className="absolute flex-center w-full text-white">
         {now} / {max}
       </div>
       {!onlyText && (
