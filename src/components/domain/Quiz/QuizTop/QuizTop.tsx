c3bd6989946af5857d9de2bbf19df0850cdc121b
Song_Minhyung <song961003@gmail.com>
2024-03-08 12:41:59 +0900

    move: Quiz 관련 domain아래 폴더로 이동함

--- src/components/domain/Quiz/QuizTop/QuizTop.tsx
+++ src/components/domain/Quiz/QuizTop/QuizTop.tsx
@@ -9,17 +9,17 @@ interface QuizTopProps {
 
 export function QuizTop({ title, nowQuizPage, maxQuizPage, nowTimer }: QuizTopProps) {
   return (
-    <div>
+    <>
       <h1 className="w-full text-2xl font-bold">
         Q{nowQuizPage}. {title}
       </h1>
-      <div className="flex flex-col items-end">
+      <div className="w-full flex flex-col items-end">
         <ProgressBar
           now={nowQuizPage}
           max={maxQuizPage}
         />
         <Timer time={nowTimer} />
       </div>
-    </div>
+    </>
   );
 }
