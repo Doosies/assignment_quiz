e49dee83797a9fa0869b50181be9f10fdd4ce355
Song_Minhyung <song961003@gmail.com>
2024-03-08 12:41:22 +0900

    fix: userAnswer가 없을경우 기본 색상이 표시돼야함

---
+++ src/components/domain/Quiz/QuizBody/hooks/useQuizAnswer.tsx
@@ -0,0 +1,21 @@
+interface useQuizAnswerParams {
+  answers: string[];
+  correctAnswer: string;
+  userAnswer?: string;
+}
+
+export function useQuizAnswer({ answers, correctAnswer, userAnswer }: useQuizAnswerParams) {
+  const isCorrectAnswer = userAnswer === correctAnswer;
+
+  const correctAnswerIndex = answers.indexOf(correctAnswer);
+  const wrongAnswerIndex = answers.indexOf(userAnswer ?? '');
+
+  const getAnswerColor = (idx: number) => {
+    if (!userAnswer) return '';
+    if (idx === correctAnswerIndex) return 'text-blue';
+    if (idx === wrongAnswerIndex) return 'text-red';
+    return '';
+  };
+
+  return { isCorrectAnswer, getAnswerColor, correctAnswerIndex, wrongAnswerIndex };
+}
