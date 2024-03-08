5f27f0736ea75617d1a9ac2072245ed7b86cab14
Song_Minhyung <song961003@gmail.com>
2024-03-08 12:45:19 +0900

    feat: QuizTop에 제목만 보여주는 옵션 추가
    - 오답노트 페이지에서는 해당 내용이 필요가 없음

--- src/components/domain/Quiz/QuizTop/QuizTop.tsx
+++ src/components/domain/Quiz/QuizTop/QuizTop.tsx
@@ -5,21 +5,24 @@ interface QuizTopProps {
   nowQuizPage: number;
   maxQuizPage: number;
   nowTimer: string;
+  onlyTitle?: boolean;
 }
 
-export function QuizTop({ title, nowQuizPage, maxQuizPage, nowTimer }: QuizTopProps) {
+export function QuizTop({ title, nowQuizPage, maxQuizPage, nowTimer, onlyTitle }: QuizTopProps) {
   return (
     <>
       <h1 className="w-full text-2xl font-bold">
         Q{nowQuizPage}. {title}
       </h1>
-      <div className="w-full flex flex-col items-end">
-        <ProgressBar
-          now={nowQuizPage}
-          max={maxQuizPage}
-        />
-        <Timer time={nowTimer} />
-      </div>
+      {!onlyTitle && (
+        <div className="w-full flex flex-col items-end">
+          <ProgressBar
+            now={nowQuizPage}
+            max={maxQuizPage}
+          />
+          <Timer time={nowTimer} />
+        </div>
+      )}
     </>
   );
 }
