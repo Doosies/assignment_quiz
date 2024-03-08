4f8474943781cf714113fddefeb10f301af6fd49
Song_Minhyung <song961003@gmail.com>
2024-03-08 19:14:19 +0900

    feat: HomePage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,27 +1,35 @@
-import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { ProgressBar } from '@components/common';
 import { Button } from '@components/common';
 import { PageLayout } from '@components/layout';
 
 export function HomePage() {
-  const [now, setNow] = useState(0);
+  const navigate = useNavigate();
+
+  const goQuizPage = () => navigate('/quiz');
+  const goWrongNotePage = () => navigate('/wrong-note');
 
   return (
-    <>
-      <PageLayout noTopBar>
-        <ProgressBar
-          now={now}
-          max={7}
-          onlyText
-        />
-        <ProgressBar
-          now={now}
-          max={7}
-        />
-        <br />
-        <Button onClick={() => setNow(now + 1)}>Increment</Button>
-      </PageLayout>
-    </>
+    <PageLayout
+      className="flex-center"
+      noTopBar
+    >
+      <div className="w-full max-w-sm h-full flex-col flex-center gap-4 ">
+        <Button
+          onClick={goQuizPage}
+          size="full"
+        >
+          퀴즈 풀기
+        </Button>
+
+        <Button
+          onClick={goWrongNotePage}
+          color="secondary"
+          size="full"
+        >
+          오답 노트
+        </Button>
+      </div>
+    </PageLayout>
   );
 }
