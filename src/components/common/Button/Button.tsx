37db79beec39dd1bec91ea57a1c800dc6e6488e6
Song_Minhyung <song961003@gmail.com>
2024-03-10 17:28:22 +0900

    feat: Button invisible prop 추가
    - 해당 옵션에 따라 숨기거나 나타낼 수 있음

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -5,18 +5,35 @@ interface ButtonProps {
   noBorder?: boolean;
   color?: 'primary' | 'secondary' | 'none';
   size?: 'xs' | 'sm' | 'md' | 'lg' | 'lg' | '2xl' | 'full';
+  invisible?: boolean;
 }
 
-export function Button({ children, onClick, className, noBorder, color = 'primary', size = 'md' }: ButtonProps) {
-  const bgColor = color === 'primary' ? 'text-white bg-blue hover:bg-blue-hover' : 'bg-white hover:bg-gray';
+const bgColors = {
+  primary: 'text-white bg-blue hover:bg-blue-hover',
+  secondary: 'text-white bg-green hover:bg-green-hover',
+  none: '',
+};
+
+export function Button({
+  children,
+  onClick,
+  className,
+  noBorder,
+  invisible = false,
+  color = 'primary',
+  size = 'md',
+}: ButtonProps) {
+  const bgColor = bgColors[color];
   const buttonSize = size === 'full' ? 'w-full' : `text-${size}`;
   const border = noBorder ? '' : 'border';
+  const visible = invisible ? 'invisible' : '';
 
   return (
     <button
       onClick={onClick}
-      className={`rounded-md p-2 px-4 transition-colors flex-center ${className}
-                  ${buttonSize} ${color !== 'none' && bgColor} ${border} `}
+      className={`rounded-md p-2 px-4 transition-colors flex-center 
+                  ${buttonSize} ${bgColor} ${border} ${visible} ${className} 
+                `}
     >
       {children}
     </button>
