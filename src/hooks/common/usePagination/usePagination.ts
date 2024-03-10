a2138c3b8f939fb22e85c0a3191483528b5d1788
Song_Minhyung <song961003@gmail.com>
2024-03-10 17:35:51 +0900

    feat: usePagenation 동작 추가
    - 첫페이지인지 확인하는 변수와 이전 페이지로 돌아가는 함수 추가

--- src/hooks/common/usePagination/usePagination.ts
+++ src/hooks/common/usePagination/usePagination.ts
@@ -7,11 +7,22 @@ interface usePaginationParams {
 export function usePagination({ nowIdx = 0, maxIdx = 0 }: usePaginationParams = {}) {
   const [nowPageNum, setNowPageNum] = useState(nowIdx);
 
+  const isFistPage = nowPageNum === 0;
   const isLastPage = nowPageNum === maxIdx;
 
   const goNextPage = () => {
+    if (isLastPage) {
+      return;
+    }
     setNowPageNum(prevPage => prevPage + 1);
   };
 
-  return { nowPageNum, isLastPage, goNextPage };
+  const goPrevPage = () => {
+    if (isFistPage) {
+      return;
+    }
+    setNowPageNum(prevPage => prevPage - 1);
+  };
+
+  return { nowPageNum, isLastPage, isFistPage, goPrevPage, goNextPage };
 }
